1f938191d98a962574dbf5652d337ef23ad797c9
Jason Underhill <jay.underhill@gmail.com>
2016-12-12 10:48:11 +0000

    setup datamapper with initial test data

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org' do
   gem 'sinatra'
+  gem 'data_mapper'
+  gem 'dm-sqlite-adapter'
+  gem 'sqlite3'
 end
 
--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Invite
+  include DataMapper::Resource
+  property :id,		Serial
+  property :code, 	String, :required => true
+  property :name,	String, :required => true
+  has 1, :rsvp,	:required => false
+end
+
+class Rsvp
+  include DataMapper::Resource
+  property :response,	Boolean,:required => true
+  property :dietary,	String
+  property :songrequest,String
+  property :responded,	DateTime, :default => DateTime.now
+  belongs_to :invite, :key => true
+end
+
+DataMapper.finalize
 
 get '/' do
   erb :index
